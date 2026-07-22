5c08cbf2e75ea23296071d55813f6031314bc8fb
GBS09645 <gangeswaran.bb@centrico.tech>
2026-07-22 19:51:12 +0530

    Chat conversation

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
 def main():
-    print("Hello from langchain-course!")
+    print("Welcome to LangChain course!! ")
+    repeat = True
+    while (repeat):
+        query = input("Ask a question : ")
+        
+        template = """
+            Could you please help user to get information about query {query}.
+            I want you to give only short answers, don't give large response.
+        """
+        prompt_template = PromptTemplate(input_variables=["query"], template=template)
+        
+        llm = ChatOpenAI(temperature=0, model="gpt-4")
+        
+        chain = prompt_template | llm
+        
+        response = chain.invoke(input={"query": query})
+        
+        print(response.content)
+
+        choice = input("Would like to exit from chat further enter YES/NO : ")
+        
+        if (choice.upper() == "YES"):
+            repeat = False
 
 
 if __name__ == "__main__":
