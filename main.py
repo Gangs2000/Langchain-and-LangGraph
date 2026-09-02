33abf5ce2ec54867c1d88b85d45ec28915cf1a7a
GBS09645 <gangeswaran.bb@centrico.tech>
2026-09-02 13:51:41 +0530

    mcp-weather-server added

--- main.py
+++ main.py
@@ -2,37 +2,37 @@ import asyncio
 from pathlib import Path
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature= 0)
+llm = ChatOpenAI(temperature=0)
 
-MCP_SERVER_ABSOLUTE_PATH = str(Path(__file__).parent / "servers" / "Math.py")
+MCP_SERVER_ABSOLUTE_PATH = str(Path(__file__).parent / "servers" / "mcp_server.py")
 
 stdio_server_params = StdioServerParameters(
-    command= "python",
-    args = [MCP_SERVER_ABSOLUTE_PATH]
+    command="python", args=[MCP_SERVER_ABSOLUTE_PATH]
 )
+
 
 async def main():
     async with stdio_client(stdio_server_params) as (read, write):
-        async with ClientSession(read_stream= read, write_stream= write) as session:
+        async with ClientSession(read_stream=read, write_stream=write) as session:
             await session.initialize()
             print("=== Session Initialized ===")
-            tools = await load_mcp_tools(session= session)
-            
-            agent = create_agent(llm, tools= tools)
-            
-            message = HumanMessage(content= "What is 90-904?")
-            
-            result = await agent.ainvoke({"messages" : [message]})
-            
+            tools = await load_mcp_tools(session=session)
+
+            agent = create_agent(llm, tools=tools)
+
+            message = HumanMessage(content="What is weather now bangalore?")
+
+            result = await agent.ainvoke({"messages": [message]})
+
             print(result["messages"][-1].content)
 
 
--- servers/mcp_server.py
+++ servers/mcp_server.py
@@ -1,27 +1,47 @@
 # math_server.py
+import os
+
+from dotenv import load_dotenv
 from mcp.server.fastmcp import FastMCP
 
+load_dotenv()
+from tavily import TavilyClient
+
 mcp = FastMCP("Math")
+
 
 @mcp.tool()
 def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+
 @mcp.tool()
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return a * b
+
 
 @mcp.tool()
 def subtract(a: int, b: int) -> int:
     """Subtract two numbers"""
     return a - b
 
+
 @mcp.tool()
 def divide(a: int, b: int) -> float:
     """Divide two numbers"""
     return a / b
+
+
+@mcp.tool()
+def search_weather(query: str) -> str:
+    """Search weather using Tavily search engine"""
+    search_results = TavilyClient(api_key=os.getenv("TAVILY_API_KEY")).search(
+        query=query
+    )["results"]
+    return "\n".join(result["content"] for result in search_results)
+
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
